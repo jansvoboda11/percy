7d0dbd4a962e210a83b153de2c3fabb25856c5dc
Jan Svoboda <github-i2kv@jansvoboda.dev>
2020-03-30 10:44:26 +0200

    Migrate to percy::variant

---
+++ cmake/PercyVariant.cmake
@@ -0,0 +1,8 @@
+include(FetchContent)
+
+FetchContent_Declare(PercyVariant
+  GIT_REPOSITORY https://github.com/jansvoboda11/percy-variant.git
+  GIT_TAG origin/master
+)
+
+FetchContent_MakeAvailable(PercyVariant)
